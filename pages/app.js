407834046ee9c85c443a866526f9f1d8e2e316b9
Thiago Augusto <thiagoggth@gmail.com>
2020-04-14 15:49:58 -0400

    Implemented using firebase, validation if the user published the status of the day

--- pages/app.js
+++ pages/app.js
@@ -1,6 +1,21 @@
+import React, { useEffect } from 'react';
 import auth0 from '../lib/auth0';
+import router from 'next/router';
+import { db } from '../lib/db';
 
 const App = (props) => {
+    useEffect(() => {
+        if (!props.isAuth) {
+            router.push('/');
+        } else if (props.forceCreate) {
+            router.push('/create-status');
+        }
+    }, []);
+
+    if (!props.isAuth || props.forceCreate) {
+        return null
+    }
+
     return (
         <>
             <h1>app</h1>
@@ -15,9 +30,19 @@ export async function getServerSideProps({ req, res }) {
     const session = await auth0.getSession(req);
 
     if (session) {
+        const todaysCheckin = await db
+            .collection('markers')
+            .doc('2020-04-14')
+            .collection('checks')
+            .doc(session.user.sub)
+            .get();
+        const todaysData = todaysCheckin.data();
+
         return {
             props: {
-                user: session.user
+                isAuth: true,
+                user: session.user,
+                forceCreate: todaysData ? false : true
             }
         }
     }
@@ -25,6 +50,7 @@ export async function getServerSideProps({ req, res }) {
 
     return {
         props: {
+            isAuth: false,
             user: {}
         }
     }
